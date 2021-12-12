b9dc209427b648c8732d42c35e8840041b587bb9
ijin <2zun@naver.com>
2021-12-12 22:11:12 +0900

    주문 , 할인 도메인 테스트

---
+++ src/test/java/hello/order/OrderServiceTest.java
@@ -0,0 +1,24 @@
+package hello.order;
+
+import hello.core.member.*;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    @Test
+    void createOrder(){
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "memberA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
