e5b19bf36998950571482407693435e1fbc495e4
ijin <2zun@naver.com>
2022-01-15 18:09:02 +0900

    스프링으로의 전환 @Configuration , @Bean 스프링이 알아서 찾아서 주입해줌

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,23 +9,28 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class AppConfig {
     // 공연 기획자 역할
-    //애플리케이션 전반을 책임짐
+    //애플리케이션 전반을 책짐
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
     }
 
-
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
 
     }
-
+    @Bean
     public MemberRepository memberRepository (){
         return new MemoryMemberRepository();
     }
+    @Bean
     public DiscountPolicy discountPolicy(){
         return new RateDiscountPolicy();
     }
---
+++ src/main/java/hello/core/member/MemberApp.java
@@ -0,0 +1,19 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class MemberApp {
+    //스프링으로전환
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println(member.getName());
+        System.out.println(findMember.getName());
+    }
+}
