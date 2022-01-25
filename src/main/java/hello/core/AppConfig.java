d6167bb4dac1ad287b657582671ba3da828bef75
ijin <2zun@naver.com>
2022-01-25 11:20:57 +0900

    스프링 핵심 원리 이해 2 - 객체지향 원리 적용

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -23,7 +23,8 @@ public class AppConfig {
 
     @Bean
     public OrderService orderService(){
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
+        return new OrderServiceImpl(
+                memberRepository(), discountPolicy());
 
     }
     @Bean
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -14,13 +14,14 @@ public class OrderServiceImpl implements OrderService{
     // private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
 
     private final MemberRepository memberRepository ;
+    private final DiscountPolicy discountPolicy ;
 
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
 
-    private final DiscountPolicy discountPolicy ;
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
